850b3cc7343f73ce24b68763239ad6e1152980da
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-05-23 20:08:15 +0530

    modified app.modules.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { User } from './auth/user.entity';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // In-memory mode using SQLite (acts like PostgreSQL for dev)
+      database: ':memory:',
+      dropSchema: true,
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
