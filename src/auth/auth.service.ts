97126af4940e8c89d30bdb721484193093762248
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-05-23 20:54:41 +0530

    updated the auth.service file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
+  async register(registerDto: RegisterDto) {
+    const { username, password } = registerDto;
+
+    // Check if user already exists
+    const existingUser = await this.userRepo.findOne({ where: { username } });
+    if (existingUser) {
+      throw new Error('User already exists');
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = this.userRepo.create({ username, password: hashedPassword });
+    return this.userRepo.save(user);
+  }
+}
