2f14cc151aa3e66979092c15b3e75bf20dee6e41
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-05-23 23:35:53 +0530

    updated in main.ts with swagger and controller to make more secure

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,23 +5,31 @@ import { LoginDto } from './dto/login.dto';
 import { UnauthorizedException } from '@nestjs/common';
 import { UseGuards, Get, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiBody({ type: RegisterDto })
+  @ApiResponse({ status: 201, description: 'User registered successfully' })
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
 
   @Post('login')
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({ status: 200, description: 'JWT token returned' })
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
   }
 
   @Get('me')
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth() // ✅ shows lock icon in Swagger
+  @ApiResponse({ status: 200, description: 'Returns current user info' })
   me(@Req() req) {
     return req.user;
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('NestJS Auth API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Use ConfigService to access PORT from .env
+  const configService = app.get(ConfigService);
+  const port = configService.get('DB_PORT');
+
+  await app.listen(port);
+  console.log(`🚀 Server is running on http://localhost:${port}`);
 }
 bootstrap();
